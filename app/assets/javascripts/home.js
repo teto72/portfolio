cc67634d70dcd452c2b629804e80e8ee1dc3306d
teto72 <tsubasa.k.729@gmail.com>
2020-06-23 13:32:08 +0900

    add:skill-name

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,17 @@
+// $(function(){
+//   function animation(){
+//     $('.fadein').each(function(){
+//       const target = $(this).offset().top;
+//       const scroll = $(window).scrollTop();
+//       const windowHeight = $(window).height();
+//       if (scroll > target - windowHeight) {
+//         $(this).css('opacity','1');
+//         $(this).css('transform','translateY(0)');
+//       }
+//     });
+//   }
+//   animation();
+//   $(window).scroll(function(){
+//     animation();
+//   });
+// });
