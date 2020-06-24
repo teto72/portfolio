29edf2e9d854ab1e7ba5a570c6b0e69996f0429b
teto72 <tsubasa.k.729@gmail.com>
2020-06-24 23:02:51 +0900

    add:portfolioモーダル

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -9,7 +9,25 @@ $(function(){
     //リンク先までの距離を取得
     let position = target.offset().top;
     //スムーススクロール
-    $("html, body").animate({scrollTop:position}, speed, "slow");
+    $("html, body").animate({scrollTop:position});
     return false;
   });
 });
+
+// 'use strict';
+// {
+//   const open = document.getElementById('open');
+//   const modal = document.getElementById('modal');
+//   const mask = document.getElementById('mask');
+
+//   open.addEventListener('click', function(){
+//     modal.classList.remove('hidden');
+//     mask.classList.remove('hidden');
+//   });
+// }
+
+$(function(){
+  $('#open').click(function(){
+    
+  })
+})
